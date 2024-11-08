f17d7a525cf8b644369947f74f1e75cab78cfaa6
灵弦 <ahzvenol@users.noreply.github.com>
2024-11-08 15:06:27 +0800

    ci:自动构建发布到gh-pages
    ci:自动构建发布到gh-pages
    
    wip:清理依赖
    
    wip:gh-pages
    
    wip:

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,31 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { Plugin as importToCDN } from 'vite-plugin-cdn-import'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 export default defineConfig(({ command }) => ({
-  plugins: [vue(), importToCDN({
-    modules: [
-      {
-        name: 'axios',
-        var: 'axios',
-        path: 'https://cdn.bootcdn.net/ajax/libs/axios/1.2.2/axios.min.js'
-      },
-    ]
-  })],
+  base: "/Wind-Song",
+  plugins: [vue()],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src")
-    }
+      "@": resolve(__dirname, "./src"),
+    },
   },
   css: {
     preprocessorOptions: {
       less: {
-        math: "always"
-      }
-    }
+        math: "always",
+      },
+    },
   },
   server: {
     port: 8888,
-    strictPort: true
+    strictPort: true,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
     sourcemap: false,
   },
   esbuild: {
-    drop: command === 'build' ? ["console", "debugger"] : [],
+    drop: command === "build" ? ["console", "debugger"] : [],
   },
-}))
+}));
